26ea7bd9a4bebedfc590230a2c62f88184415b25
KKBUGHUNTER <91019132+kkbughunter@users.noreply.github.com>
2025-03-10 22:10:07 +0530

    Add files via upload

---
+++ GoogleAndGmailAuth/README.md
@@ -0,0 +1,5 @@
+# Google & Gmail Auth
+
+```bash
+flutter pub add firebase_core flutter_signin_button firebase_auth google_sign_in
+```
---
+++ GoogleAndGmailAuth/lib/screens/register.dart
@@ -0,0 +1,223 @@
+import '/authmanagement/auth_manage.dart';
+import 'package:flutter/material.dart';
+
+class AppRegisterPage extends StatefulWidget {
+  const AppRegisterPage({super.key});
+
+  @override
+  State<AppRegisterPage> createState() => _AppRegisterPageState();
+}
+
+class _AppRegisterPageState extends State<AppRegisterPage> {
+  bool _isPasswordVisible = true;
+  final _signFormKey = GlobalKey<FormState>();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register Account"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Form(
+          key: _signFormKey,
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 200,
+                child: Image.asset("assets/images/sign_up.png"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(fontSize: 25, color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (_emailController) {
+                    if (_emailController!.isEmpty) {
+                      return "Email is required";
+                    }
+                    return null;
+                  },
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: "User Name",
+                    hintText: "Samuel",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (_emailController) {
+                    if (_emailController!.isEmpty) {
+                      return "Email is required";
+                    }
+                    return null;
+                  },
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "admin@gmail.com",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (_passwordController) {
+                    if (_passwordController!.length < 6) {
+                      return "Password lengith must be at least 6 characters";
+                    } else if (_passwordController !=
+                        _confirmPasswordController.text.trim()) {
+                      return "Password does not match";
+                    }
+                    return null;
+                  },
+                  controller: _passwordController,
+                  obscureText: _isPasswordVisible,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "********",
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.password,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  validator: (_confirmPasswordController) {
+                    if (_confirmPasswordController!.length < 6) {
+                      return "Password lengith must be at least 6 characters";
+                    }
+                    return null;
+                  },
+                  controller: _confirmPasswordController,
+                  obscureText: _isPasswordVisible,
+                  decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    hintText: "********",
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.password,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(top: 4, left: 50, right: 50),
+                child: registerButton(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget registerButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        if (_signFormKey.currentState!.validate()) {
+          try {
+            await AuthManage().register(
+              _emailController.text.trim(),
+              _passwordController.text.trim(),
+            );
+            Navigator.pop(context);
+          } catch (e) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                backgroundColor: Colors.red,
+                content: Text(
+                  "The email address is already in use.",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            );
+          }
+        }
+      },
+      child:
+          Text("Submit", style: TextStyle(fontSize: 20, color: Colors.white)),
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(0, 60),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+        backgroundColor: Colors.blue[900],
+      ),
+    );
+  }
+}
